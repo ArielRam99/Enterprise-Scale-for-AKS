9b0b998d4edabb6f5be015db2c6f8a52a185b969
ArielRam99 <ariel.ramirez@microsoft.com>
2021-10-19 16:14:43 -0700

    deploy 04

--- Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/07-AKS-cluster/postgres.tf
+++ Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/07-AKS-cluster/postgres.tf
@@ -22,77 +22,77 @@
 
 #data "azurerm_client_config" "current" {}
 
-resource "azurerm_resource_group" "postgressql_rg" {
-  name     = "postgressql-rg"
-  location = var.location #eastus make default
-}
-
-resource "azurerm_postgresql_server" "postgresql_server" {
-  name                = "postgresql-00000001"
-  location            = azurerm_resource_group.postgressql_rg.location
-  resource_group_name = azurerm_resource_group.postgressql_rg.name
-
-  administrator_login          = "psqladmin"
-  administrator_login_password = "H@Sh1CoR3!"
-  auto_grow_enabled            = true
-  backup_retention_days        = 7
-  geo_redundant_backup_enabled = false
-  sku_name                     = "GP_Gen5_2"
-  ssl_enforcement_enabled      = true
-  storage_mb                   = 51200
-  version                      = "11"
-}
-
-resource "azurerm_private_endpoint" "postgressql_pe" {
-  name                = "postgres-pe"
-  location            = azurerm_resource_group.postgressql_rg.location
-  resource_group_name = azurerm_resource_group.postgressql_rg.name
-  subnet_id           = data.terraform_remote_state.existing-lz.outputs.aks_subnet_id
-
-  private_service_connection {
-    name                           = "postgresql-connection"
-    is_manual_connection           = false
-    private_connection_resource_id = azurerm_postgresql_server.postgresql_server.id
-    subresource_names              = ["postgresqlServer"]
-  }
-}
-
-#output "aks_subnet_id" {
-#  value = azurerm_subnet.aks.id
+#resource "azurerm_resource_group" "postgressql_rg" {
+#  name     = "postgressql-rg"
+#  location = var.location #eastus make default
 #}
-
-resource "azurerm_private_dns_zone" "postgressql_dns" {
-  name                = var.postgressql_private_dns_zone_name
-  resource_group_name = azurerm_resource_group.postgressql_rg.name
-}
-
-# Needed for Jumpbox to resolve postgressql using a private endpoint and private dns zone
-resource "azurerm_private_dns_zone_virtual_network_link" "hub_postgressql" {
-  name                  = "hub_to_postgressql"
-  resource_group_name   = azurerm_resource_group.postgressql_rg.name
-  private_dns_zone_name = azurerm_private_dns_zone.postgressql_dns.name
-  virtual_network_id    = data.terraform_remote_state.existing-hub.outputs.hub_vnet_id
-}
-
-resource "azurerm_role_assignment" "postgres-to-dnszone" {
-  scope                = azurerm_private_dns_zone.postgressql_dns.id
-  role_definition_name = "Private DNS Zone Contributor"
-  principal_id         = azurerm_user_assigned_identity.mi-aks-cp.principal_id
-}
-
-output "postgressql_private_zone_id" {
-  value = azurerm_private_dns_zone.postgressql_dns.id
-}
-output "postgressql_private_zone_name" {
-  value = azurerm_private_dns_zone.postgressql_dns.name
-}
-
-################################
-
-variable "location" {
-    default = "EastUS"
-}
-
-variable "postgressql_private_dns_zone_name" {
-    default = "privatelink.postgres.database.azure.com"
-}
+#
+#resource "azurerm_postgresql_server" "postgresql_server" {
+#  name                = "postgresql-00000001"
+#  location            = azurerm_resource_group.postgressql_rg.location
+#  resource_group_name = azurerm_resource_group.postgressql_rg.name
+#
+#  administrator_login          = "psqladmin"
+#  administrator_login_password = "H@Sh1CoR3!"
+#  auto_grow_enabled            = true
+#  backup_retention_days        = 7
+#  geo_redundant_backup_enabled = false
+#  sku_name                     = "GP_Gen5_2"
+#  ssl_enforcement_enabled      = true
+#  storage_mb                   = 51200
+#  version                      = "11"
+#}
+#
+#resource "azurerm_private_endpoint" "postgressql_pe" {
+#  name                = "postgres-pe"
+#  location            = azurerm_resource_group.postgressql_rg.location
+#  resource_group_name = azurerm_resource_group.postgressql_rg.name
+#  subnet_id           = data.terraform_remote_state.existing-lz.outputs.aks_subnet_id
+#
+#  private_service_connection {
+#    name                           = "postgresql-connection"
+#    is_manual_connection           = false
+#    private_connection_resource_id = azurerm_postgresql_server.postgresql_server.id
+#    subresource_names              = ["postgresqlServer"]
+#  }
+#}
+#
+##output "aks_subnet_id" {
+##  value = azurerm_subnet.aks.id
+##}
+#
+#resource "azurerm_private_dns_zone" "postgressql_dns" {
+#  name                = var.postgressql_private_dns_zone_name
+#  resource_group_name = azurerm_resource_group.postgressql_rg.name
+#}
+#
+## Needed for Jumpbox to resolve postgressql using a private endpoint and private dns zone
+#resource "azurerm_private_dns_zone_virtual_network_link" "hub_postgressql" {
+#  name                  = "hub_to_postgressql"
+#  resource_group_name   = azurerm_resource_group.postgressql_rg.name
+#  private_dns_zone_name = azurerm_private_dns_zone.postgressql_dns.name
+#  virtual_network_id    = data.terraform_remote_state.existing-hub.outputs.hub_vnet_id
+#}
+#
+#resource "azurerm_role_assignment" "postgres-to-dnszone" {
+#  scope                = azurerm_private_dns_zone.postgressql_dns.id
+#  role_definition_name = "Private DNS Zone Contributor"
+#  principal_id         = azurerm_user_assigned_identity.mi-aks-cp.principal_id
+#}
+#
+#output "postgressql_private_zone_id" {
+#  value = azurerm_private_dns_zone.postgressql_dns.id
+#}
+#output "postgressql_private_zone_name" {
+#  value = azurerm_private_dns_zone.postgressql_dns.name
+#}
+#
+#################################
+#
+#variable "location" {
+#    default = "EastUS"
+#}
+#
+#variable "postgressql_private_dns_zone_name" {
+#    default = "privatelink.postgres.database.azure.com"
+#}
