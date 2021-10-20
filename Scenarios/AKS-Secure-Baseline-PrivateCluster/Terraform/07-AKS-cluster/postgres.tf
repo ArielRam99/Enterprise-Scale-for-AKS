aba77d8384b6151ff19e038deb626363c33ff26f
ArielRam99 <ariel.ramirez@microsoft.com>
2021-10-20 15:05:50 -0700

    deploy pg

--- Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/07-AKS-cluster/postgres.tf
+++ Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/07-AKS-cluster/postgres.tf
@@ -22,40 +22,40 @@
 
 #data "azurerm_client_config" "current" {}
 
-#resource "azurerm_resource_group" "postgressql_rg" {
-#  name     = "postgressql-rg"
-#  location = var.location #eastus make default
-#}
-#
-#resource "azurerm_postgresql_server" "postgresql_server" {
-#  name                = "postgresql-00000001"
-#  location            = azurerm_resource_group.postgressql_rg.location
-#  resource_group_name = azurerm_resource_group.postgressql_rg.name
-#
-#  administrator_login          = var.administrator_login
-#  administrator_login_password = var.administrator_login_password
-#  auto_grow_enabled            = var.auto_grow_enabled
-#  backup_retention_days        = var.backup_retention_days
-#  geo_redundant_backup_enabled = var.geo_redundant_backup_enabled
-#  sku_name                     = var.sku_name
-#  ssl_enforcement_enabled      = var.ssl_enforcement_enabled
-#  storage_mb                   = var.storage_mb
-#  version                      = 11
-#}
-#
-#resource "azurerm_private_endpoint" "postgressql_pe" {
-#  name                = "postgres-pe"
-#  location            = azurerm_resource_group.postgressql_rg.location
-#  resource_group_name = azurerm_resource_group.postgressql_rg.name
-#  subnet_id           = data.terraform_remote_state.existing-lz.outputs.aks_subnet_id
-#
-#  private_service_connection {
-#    name                           = "postgresql-connection"
-#    is_manual_connection           = false
-#    private_connection_resource_id = azurerm_postgresql_server.postgresql_server.id
-#    subresource_names              = ["postgresqlServer"]
-#  }
-#}
+resource "azurerm_resource_group" "postgressql_rg" {
+  name     = "postgressql-rg"
+  location = var.location #eastus make default
+}
+
+resource "azurerm_postgresql_server" "postgresql_server" {
+  name                = "postgresql-00000001"
+  location            = azurerm_resource_group.postgressql_rg.location
+  resource_group_name = azurerm_resource_group.postgressql_rg.name
+
+  administrator_login          = var.administrator_login
+  administrator_login_password = var.administrator_login_password
+  auto_grow_enabled            = var.auto_grow_enabled
+  backup_retention_days        = var.backup_retention_days
+  geo_redundant_backup_enabled = var.geo_redundant_backup_enabled
+  sku_name                     = var.sku_name
+  ssl_enforcement_enabled      = var.ssl_enforcement_enabled
+  storage_mb                   = var.storage_mb
+  version                      = 11
+}
+
+resource "azurerm_private_endpoint" "postgressql_pe" {
+  name                = "postgres-pe"
+  location            = azurerm_resource_group.postgressql_rg.location
+  resource_group_name = azurerm_resource_group.postgressql_rg.name
+  subnet_id           = data.terraform_remote_state.existing-lz.outputs.aks_subnet_id
+
+  private_service_connection {
+    name                           = "postgresql-connection"
+    is_manual_connection           = false
+    private_connection_resource_id = azurerm_postgresql_server.postgresql_server.id
+    subresource_names              = ["postgresqlServer"]
+  }
+}
 
 #output "aks_subnet_id" {
 #  value = azurerm_subnet.aks.id
@@ -104,42 +104,42 @@
 
 ################################
 
-#variable "location" {
-#    default = "EastUS"
-#}
-#
-#variable "postgressql_private_dns_zone_name" {
-#    default = "privatelink.postgres.database.azure.com"
-#}
-#
-#variable "administrator_login" {
-#    default = "psqladmin"
-#}
-#
-#variable "administrator_login_password" {
-#    default = "H@Sh1CoR3!"
-#}
-#
-#variable "auto_grow_enabled" {
-#    default = true
-#}
-#
-#variable "backup_retention_days" {
-#    default = 7
-#}
-#
-#variable "geo_redundant_backup_enabled" {
-#    default = false
-#}
-#
-#variable "sku_name" {
-#    default = "GP_Gen5_2"
-#}
-#
-#variable "ssl_enforcement_enabled" {
-#    default = true
-#}
-#
-#variable "storage_mb" {
-#    default = 51200
-#}
+variable "location" {
+    default = "EastUS"
+}
+
+variable "postgressql_private_dns_zone_name" {
+    default = "privatelink.postgres.database.azure.com"
+}
+
+variable "administrator_login" {
+    default = "psqladmin"
+}
+
+variable "administrator_login_password" {
+    default = "H@Sh1CoR3!"
+}
+
+variable "auto_grow_enabled" {
+    default = true
+}
+
+variable "backup_retention_days" {
+    default = 7
+}
+
+variable "geo_redundant_backup_enabled" {
+    default = false
+}
+
+variable "sku_name" {
+    default = "GP_Gen5_2"
+}
+
+variable "ssl_enforcement_enabled" {
+    default = true
+}
+
+variable "storage_mb" {
+    default = 51200
+}
